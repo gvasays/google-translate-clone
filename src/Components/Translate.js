cfd1cc5becd9afd73d0b8d8cc2c0c04eb5ebdf89
gvasays <71869080+gvasays@users.noreply.github.com>
2024-03-20 09:47:25 -0700

    added textbox

--- src/Components/Translate.js
+++ src/Components/Translate.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {
+  FormField,
+  TextArea,
+} from 'semantic-ui-react';
 
 export default function Translate() {
   return (
     <div>
       <div className="app-header">
-        <h2> My bootleg ass translate app. </h2>
+        <h2 className="header"> My bootleg ass translate app. </h2>
+
+      </div>
+      <div className='app-body'>
+        <div>
+        <FormField
+          control={TextArea}
+          label='About'
+          placeholder='Tell us more about you...'
+        />
+        </div>
 
       </div>
     </div>
